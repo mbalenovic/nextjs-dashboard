7fb80d1bbca1d3aa2a89d1149fcc71615c66c54c
Mile <mile.balenovic@conceptboard.com>
2023-11-26 21:22:00 +0100

    if invoice render form fix

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { InvoiceForm } from "@/app/lib/definitions";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -18,7 +19,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      {invoice && <Form invoice={invoice} customers={customers} />}
     </main>
   );
 }
